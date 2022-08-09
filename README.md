307da4b198f8a48cfc72fca17ebbc8136f072abc
Michael Beckler <beckler@hey.com>
2022-08-09 18:52:53 -0400

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pirate-midi-rs
+
+
+
+## Description
+
 Rust library for sending serial commands to Pirate MIDI Bridge devices
+
+## Usage
+
+Make sure your bridge device is connected via USB before running a command.
+
+```rust
+    // create our test device
+    let device = PirateMIDIDevice::new();
+
+    // get device details
+    let response = device.send(Command::Check).unwrap(); // use a `match` instead of `.unwrap()`
+
+    // enter the bootloader
+    // the device will detach and then reattach in bootloader mode, and the serial port will no longer be available.
+    // will return `ok`, but any subsequent commands will throw an error
+    match PirateMIDIDevice::new().send(Command::Control(ControlArgs::EnterBootloader)) {
+        Ok(_) => Ok(()),
+        Err(err) => panic!("unable to enter bootloader: {}", err),
+    }
+```
---
+++ src/models/check.rs
@@ -0,0 +1,41 @@
+use serde::Deserialize;
+
+#[derive(Default, Debug, Clone, Deserialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct CheckResponse {
+    #[serde(alias = "uId")]
+    pub uid: String,
+    pub device_model: String,
+    pub firmware_version: String,
+    pub hardware_version: String,
+    pub device_name: String,
+    pub profile_id: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{env, path::PathBuf};
+
+    use crate::check::CheckResponse;
+
+    #[test]
+    fn test_parsing_global_messages() {
+        match env::var("CARGO_MANIFEST_DIR") {
+            Ok(path) => {
+                let test_file_path =
+                    PathBuf::from(format!("{path}/resources/test/check-example1.json"));
+                println!("manifest: {}", test_file_path.display());
+                let contents =
+                    std::fs::read_to_string(&test_file_path).expect("unable to read file");
+                let response: Result<CheckResponse, serde_json::Error> =
+                    serde_json::from_str(&contents);
+                match response {
+                    // if we didn't panic, we passed
+                    Ok(result) => println!("{:?}", result),
+                    Err(e) => panic!("parsing of response failed! - {:?}", e),
+                };
+            }
+            Err(err) => panic!("unable to load CARGO_MANIFEST_DIR: {:?}", err),
+        }
+    }
+}
